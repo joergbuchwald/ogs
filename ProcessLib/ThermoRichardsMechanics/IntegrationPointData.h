b84fe5e9ebc0b46789095d0d4e8488224febe483
Wenqing Wang <wenqing.wang@ufz.de>
2020-10-16 15:18:06 +0200

    [Process/TRM] Added local assembler

---
+++ ProcessLib/ThermoRichardsMechanics/IntegrationPointData.h
@@ -0,0 +1,166 @@
+/**
+ * \file
+ * \copyright
+ * Copyright (c) 2012-2020, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "MaterialLib/SolidModels/LinearElasticIsotropic.h"
+#include "MathLib/KelvinVector.h"
+#include "MathLib/LinAlg/Eigen/EigenMapTools.h"
+
+namespace ProcessLib
+{
+namespace ThermoRichardsMechanics
+{
+template <typename BMatricesType, typename ShapeMatrixTypeDisplacement,
+          typename ShapeMatricesTypePressure, int DisplacementDim, int NPoints>
+struct IntegrationPointData final
+{
+    explicit IntegrationPointData(
+        MaterialLib::Solids::MechanicsBase<DisplacementDim> const&
+            solid_material)
+        : solid_material(solid_material),
+          material_state_variables(
+              solid_material.createMaterialStateVariables())
+    {
+        // Initialize current time step values
+        static const int kelvin_vector_size =
+            MathLib::KelvinVector::KelvinVectorDimensions<
+                DisplacementDim>::value;
+        sigma_eff.setZero(kelvin_vector_size);
+        sigma_sw.setZero(kelvin_vector_size);
+        eps.setZero(kelvin_vector_size);
+
+        // Previous time step values are not initialized and are set later.
+        eps_prev.resize(kelvin_vector_size);
+        sigma_eff_prev.resize(kelvin_vector_size);
+    }
+
+    typename ShapeMatrixTypeDisplacement::template MatrixType<
+        DisplacementDim, NPoints * DisplacementDim>
+        N_u_op;
+    typename BMatricesType::KelvinVectorType sigma_eff, sigma_eff_prev;
+    typename BMatricesType::KelvinVectorType sigma_sw, sigma_sw_prev;
+    // Total strain
+    typename BMatricesType::KelvinVectorType eps, eps_prev;
+
+    typename ShapeMatrixTypeDisplacement::NodalRowVectorType N_u;
+    typename ShapeMatrixTypeDisplacement::GlobalDimNodalMatrixType dNdx_u;
+
+    typename ShapeMatricesTypePressure::NodalRowVectorType N_p;
+    typename ShapeMatricesTypePressure::GlobalDimNodalMatrixType dNdx_p;
+
+    typename ShapeMatricesTypePressure::GlobalDimVectorType v_darcy;
+
+    double saturation = std::numeric_limits<double>::quiet_NaN();
+    double saturation_prev = std::numeric_limits<double>::quiet_NaN();
+    double porosity = std::numeric_limits<double>::quiet_NaN();
+    double porosity_prev = std::numeric_limits<double>::quiet_NaN();
+    double transport_porosity = std::numeric_limits<double>::quiet_NaN();
+    double transport_porosity_prev = std::numeric_limits<double>::quiet_NaN();
+    double dry_density_solid = std::numeric_limits<double>::quiet_NaN();
+    double dry_density_pellet_saturated =
+        std::numeric_limits<double>::quiet_NaN();
+    double dry_density_pellet_unsaturated =
+        std::numeric_limits<double>::quiet_NaN();
+
+    MaterialLib::Solids::MechanicsBase<DisplacementDim> const& solid_material;
+    std::unique_ptr<typename MaterialLib::Solids::MechanicsBase<
+        DisplacementDim>::MaterialStateVariables>
+        material_state_variables;
+    double integration_weight;
+
+    void pushBackState()
+    {
+        eps_prev = eps;
+        sigma_eff_prev = sigma_eff;
+        sigma_sw_prev = sigma_sw;
+        saturation_prev = saturation;
+        porosity_prev = porosity;
+        transport_porosity_prev = transport_porosity;
+        material_state_variables->pushBackState();
+    }
+
+    MathLib::KelvinVector::KelvinMatrixType<DisplacementDim>
+    computeElasticTangentStiffness(
+        double const t,
+        ParameterLib::SpatialPosition const& x_position,
+        double const dt,
+        double const temperature)
+    {
+        using KV = MathLib::KelvinVector::KelvinVectorType<DisplacementDim>;
+        auto const null_state = solid_material.createMaterialStateVariables();
+        KV const zero = KV::Zero();
+        auto&& solution = solid_material.integrateStress(
+            t, x_position, dt, zero, zero, zero, *null_state, temperature);
+
+        if (!solution)
+        {
+            OGS_FATAL("Computation of elastic tangent stiffness failed.");
+        }
+
+        MathLib::KelvinVector::KelvinMatrixType<DisplacementDim> C =
+            std::move(std::get<2>(*solution));
+
+        return C;
+    }
+
+    template <typename DisplacementVectorType>
+    typename BMatricesType::KelvinMatrixType updateConstitutiveRelation(
+        double const t,
+        ParameterLib::SpatialPosition const& x_position,
+        double const dt,
+        DisplacementVectorType const& /*u*/,
+        double const temperature)
+    {
+        auto&& solution = solid_material.integrateStress(
+            t, x_position, dt, eps_prev, eps, sigma_eff_prev,
+            *material_state_variables, temperature);
+
+        if (!solution)
+        {
+            OGS_FATAL("Computation of local constitutive relation failed.");
+        }
+
+        MathLib::KelvinVector::KelvinMatrixType<DisplacementDim> C;
+        std::tie(sigma_eff, material_state_variables, C) = std::move(*solution);
+
+        return C;
+    }
+
+    template <typename DisplacementVectorType>
+    typename BMatricesType::KelvinMatrixType updateConstitutiveRelationThermal(
+        double const t,
+        ParameterLib::SpatialPosition const& x_position,
+        double const dt,
+        DisplacementVectorType const& /*u*/,
+        double const T,
+        MathLib::KelvinVector::KelvinVectorType<DisplacementDim> const&
+            dthermal_strain)
+    {
+        auto&& solution = solid_material.integrateStress(
+            t, x_position, dt, eps_prev, eps - dthermal_strain, sigma_eff_prev,
+            *material_state_variables, T);
+
+        if (!solution)
+            OGS_FATAL("Computation of local constitutive relation failed.");
+
+        MathLib::KelvinVector::KelvinMatrixType<DisplacementDim> C;
+        std::tie(sigma_eff, material_state_variables, C) = std::move(*solution);
+
+        return C;
+    }
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+};
+
+}  // namespace ThermoRichardsMechanics
+}  // namespace ProcessLib
