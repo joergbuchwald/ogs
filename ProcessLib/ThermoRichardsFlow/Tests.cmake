5aab27790c70ffc13d5ac833b9fdb110eb5901db
joergbuchwald <joerg.buchwald@ufz.de>
2020-10-28 17:58:31 +0100

    add tests

--- ProcessLib/ThermoRichardsFlow/Tests.cmake
+++ ProcessLib/ThermoRichardsFlow/Tests.cmake
@@ -3,6 +3,20 @@ if (NOT OGS_USE_MPI)
 endif()
 
 AddTest(
+    NAME ThermoRichardsFlow_PressureDiffusionTemperatureDiffusion
+    PATH ThermoRichardsFlow/HT/SimpleSynthetics
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS PressureDiffusionTemperatureDiffusion.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    RUNTIME 1
+    DIFF_DATA
+    PressureDiffusionTemperatureDiffusion_expected.vtu PressureDiffusionTemperatureDiffusion_ts_1_t_1.000000.vtu T T 1e-8 1e-8
+    PressureDiffusionTemperatureDiffusion_expected.vtu PressureDiffusionTemperatureDiffusion_ts_1_t_1.000000.vtu p p 1e-8 1e-8
+    PressureDiffusionTemperatureDiffusion_expected.vtu PressureDiffusionTemperatureDiffusion_ts_1_t_1.000000.vtu darcy_velocity darcy_velocity 1e-8 1e-8
+)
+AddTest(
     NAME ThermoRichardsFlow_OgataBanks
     PATH ThermoRichardsFlow/OgataBanks
     EXECUTABLE ogs
